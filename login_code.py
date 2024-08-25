676e91d6c9be142e80edf3869705c5bdc370c739
Bethan Thurgood <thebluebark@gmail.com>
2024-08-25 22:58:16 +0100

    made connection to database

--- login_code.py
+++ login_code.py
@@ -1,6 +1,16 @@
 # login code
 import tkinter as Tk
+import mysql.connector 
 
+# Setting up database
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="standardUser",
+    password="StandardPassword123!",
+    database="ComputerScienceNEA"
+)
+
+print(mydb)
 class Account:
     def __init__(self, username, password):
         self.username = username
@@ -12,3 +22,5 @@ class Account:
 def createAccount(username, password):
     print(username, password)
     return Account(username, password)
+
+mydb.close()
