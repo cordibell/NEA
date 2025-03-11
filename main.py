f2e450be694ff1134c7250ba0f339d7a6bdb67cd
Bethan Thurgood <bethan@thurgood.org.uk>
2025-03-11 09:39:44 +0000

    added more formal comments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # main file, made 5/9/24
 
+# importing other modules 
 import login_gui
 import login_code
 
@@ -19,7 +20,7 @@ import calculate_optimal_time
 
 import mysql.connector
 
-# connecting to database
+# connecting to database using mysql connector library
 
 mydb = mysql.connector.connect(
     host="10.0.2.214",
@@ -29,17 +30,29 @@ mydb = mysql.connector.connect(
 )
 
 
-# logging in
+# logging in commands
 
 class LoginCommands: # encapsulates methods to collect & process data from login menu
+    '''
+    This class encapsulates everything to do with the login page, such as the window & its functionality.
+    '''
     def __init__(self, mydb):
         self.mydb = mydb
         self.is_logged_in = False
 
     def set_login(self, login):
+        '''
+        This method sets the login attribute to be the Tkinter login window.
+        '''
         self.login = login 
 
     def collect_login(self): # collects data from login to existing account text fields
+        '''
+        This method is called when the "Confirm login" button is clicked.
+        This method collects the data from the login to an existing account entries on the Login menu. 
+        It then verifies that the user's username & password are correct.
+        If the details are correct, it takes the user to the "Join an event" menu
+        '''
         self.existing_username = self.login.get_existing_username()
         self.existing_password = self.login.get_existing_password()
         self.is_logged_in = login_code.validate_login(self.existing_username, self.existing_password, self.mydb)
@@ -49,12 +62,23 @@ class LoginCommands: # encapsulates methods to collect & process data from login
             self.login.incorrect_password()
     
     def load_join_menu(self): # loads next menu if username matches password & withdraws current menu
+        '''
+        This method instantiates an object of class "JoinEventCommands".
+        Next, it hides the login menu.
+        It then collects the "join_event" window created by Tkinter, in "join_events_gui".
+        Lastly, it sets this window to be an attribute of the object instantiated earlier in the method.
+        '''
         joinEventCommand = JoinEventCommands(mydb, self.existing_username)
         self.login.login_window.withdraw()
         join_event = join_events_gui.join_events_menu(login.login_window, joinEventCommand.collect_join_event, joinEventCommand.collect_host_event)
         joinEventCommand.set_join_event(join_event)
 
     def collect_new_account(self): # collects data from new account text fields
+        '''
+        This method is called when the "Create account" button is clicked.
+        It collects the username/password from their respective entries on the Login window
+        Then, it validates & stores this information in the database by calling a function called "saved_to_db.
+        '''
         username = self.login.get_new_username()
         password = self.login.get_new_password()
         saved_to_db = login_code.saving_new_account(username, password, self.mydb)
@@ -71,14 +95,28 @@ class LoginCommands: # encapsulates methods to collect & process data from login
 
 
 class JoinEventCommands: # encapsulates methods to collect & process data from join event menu
+    '''
+    This class encapsulates everything to do with the "Join Event" menu, such as its functionality & the window.
+    '''
     def __init__(self, mydb, username):
         self.mydb = mydb
         self.username = username
     
     def set_join_event(self, join_event):
+        '''
+        This method sets the attribute "join_event" to be an object called "join_event".
+        This object contains everything to do with the "Join event" window.
+        '''
         self.join_event = join_event
         
     def collect_join_event(self): # collects data from join event code text field
+        '''
+        This method is called when the "Join Event" confirmation button is clicked.
+        It collects the code that was inputted into the entry box.
+        Then, it verifies if the id exists in the database.
+        If the ID does exist, it checks if the user is already in the event by calling the function "check_username_in_event_members".
+        If the ID exists, it takes the user to the visual calendar menu.
+        '''
         self.eventID = self.join_event.get_join_event_code()
         id_exists = join_events_code.find_event_id(self.eventID, self.mydb)
         if id_exists:
@@ -88,6 +126,13 @@ class JoinEventCommands: # encapsulates methods to collect & process data from j
             self.join_event.invalid_code()
         
     def collect_host_event(self): # collects data from hosting event inputs
+        '''
+        This method is called when the "Host event" confirmation button is clicked.
+        Firstly, it collects all of the inputs from the entry boxes & buttons on the page.
+        Then, it generates a unique code for that event.
+        Next, it validates the user's inputs against the criteria (e.g. correct format, length in range).
+        If the event is valid, it displays the uniquely generated code to the user.
+        '''
         selected_timeframe = self.join_event.get_timeframe_var()
         timeframe = join_events_code.find_timeframe(selected_timeframe)
         start_date = self.join_event.get_start_date()
@@ -107,6 +152,12 @@ class JoinEventCommands: # encapsulates methods to collect & process data from j
             self.join_event.invalid_title_length()
 
     def load_calendar_menu(self): # loads the calendar menu & hides join event menu
+        '''
+        This method loads the calendar menu.
+        It first instantiates an object of the CalendarCommands class, then hides the "join event" window.
+        Next, it collects an object containing all of the Tkinter widget information. 
+        Then, it sets the eventID attribute, and the calendar_menu attribute.
+        '''
         calendarCommand = CalendarCommands(self.mydb, self.username, self.eventID)
         self.join_event.join_events_window.withdraw()
         calendar_menu = calendar_gui.calendar_menu(login.login_window, calendarCommand.load_create_event_menu, calendarCommand.load_preferred_time_menu, calendarCommand.load_optimal_time_menu)
@@ -114,27 +165,58 @@ class JoinEventCommands: # encapsulates methods to collect & process data from j
         calendarCommand.set_calendar_menu(calendar_menu)
 
 class CalendarCommands:
+    '''
+    This class encapsulates all of the information to do with the calendar menu, such as its window & its functionality.
+    '''
     def __init__(self, mydb, username, eventID):
         self.mydb = mydb
         self.username = username
         self.eventID = eventID
 
     def set_calendar_menu(self, calendar_menu):
+        '''
+        This method sets the attribute "calendar_menu" to contain the object that stores all information about the calendar window.
+        '''
         self.calendar_menu = calendar_menu
 
     def load_create_event_menu(self): # loads create event menu & hides calendar menu
+        '''
+        This method is called when the "Add an event" button is clicked.
+        It first instantiates an object of the "CreateEventCommands" class.
+        Then, it hides the calendar menu. 
+        Next, it collects an object that contains all the information about the "Create event" window from the "create_event_menu" function.
+        Lastly, it sets this object as an attribute of the createEventCommand object.
+        This causes the "Create an event" menu to appear on-screen.
+        '''
         createEventCommand = CreateEventCommands(self.mydb, self.calendar_menu, self.username)
         self.calendar_menu.calendar_window.withdraw()
         create_event_menu = create_event_gui.create_event_menu(login.login_window, createEventCommand.create_event, createEventCommand.load_calendar_menu)
         createEventCommand.set_create_event_menu(create_event_menu)
     
     def load_preferred_time_menu(self): # loads preferred time menu, hides calendar window
+        '''
+        This method is called when the "Set preferred times" button is clicked.
+        It firstly instantiates an object of the "SetPreferredTimeCommands" class.
+        Then, it hides the calendar menu.
+        Next, it collects an object from the "Set preferred times" menu which contains all the information about this window.
+        It saves this object as the "set_preferred_time_menu" attribute of the "setPreferredTimeCommand" object.
+        This causes the "Set preferred time" menu to appear on-screen.
+        '''
         setPreferredTimeCommand = SetPreferredTimeCommands(self.mydb, self.calendar_menu, self.username)
         self.calendar_menu.calendar_window.withdraw()
         set_preferred_time_menu = set_preferred_time_gui.set_preferred_times_menu(login.login_window, setPreferredTimeCommand.set_preferred_time, setPreferredTimeCommand.load_calendar_menu)
         setPreferredTimeCommand.set_set_preferred_time_menu(set_preferred_time_menu)
     
     def load_optimal_time_menu(self): # loads optimal time menu, hides calendar window
+        '''
+        This method is called when the "View optimal times" button is clicked.
+        It first instantiates an object of the "OptimalTimeCommands" class.
+        Then, it hides the calendar window.
+        Next, it collects a list of the best times by calling a function in the "calculate_optimal_time" module.
+        It then collects an object from the function "optimal_time_menu" which contains information about the implementation of the GUI.
+        Lastly, it sets this object as an attribute of the first object made in the function.
+        This causes the "Choose Optimal Time" menu to appear on-screen.
+        '''
         optimalTimeCommand = OptimalTimeCommands(self.mydb, self.calendar_menu)
         self.calendar_menu.calendar_window.withdraw()
         list_of_best_times = calculate_optimal_time.get_users_in_event(self.eventID, self.mydb)
